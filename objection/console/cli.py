922d1167bd9aac2dad43132c06df4809a5044e5a
Jeroen Beckers <me.githbub@dauntless.be>
2025-09-05 14:12:15 +0200

    Change pause argument

--- objection/console/cli.py
+++ objection/console/cli.py
@@ -59,7 +59,7 @@ def get_agent() -> Agent:
 @click.option('--debugger', required=False, default=False, is_flag=True, help='Enable the Chrome debug port.')
 @click.option('--uid', required=False, default=None, help='Specify the uid to run as (Android only).')
 def cli(network: bool, host: str, port: int, api_host: str, api_port: int,
-        name: str, gadget: str, serial: str, debug: bool, no_pause: bool,
+        name: str, gadget: str, serial: str, debug: bool, pause: bool,
         foremost: bool, debugger: bool, uid: int) -> None:
     """
         \b
